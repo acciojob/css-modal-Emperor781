d89ca1d5431d0b96d693558fcb767c0d1ed41c10
Cyril Cristo Johns <95585437+Emperor781@users.noreply.github.com>
2026-05-16 19:40:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let modal=document.querySelector('#modal')
+let btn=document.querySelector('#openModal');
+let body=document.querySelector('body');
+
+// body.style.backgroundColor='red'
+btn.addEventListener('click',()=>{
+	modal.showModal();
+	setTimeout(()=> body.addEventListener('click', ()=> modal.close()), 3000)
+	
+	
+})
+
+
