996d075692167f49bb3019f67e08eb4eb44fc2cc
Cyril Cristo Johns <95585437+Emperor781@users.noreply.github.com>
2026-05-29 11:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //your JS code here. If required.
 let modal=document.querySelector('#modal')
 let btn=document.querySelector('#openModal');
-let body=document.querySelector('body');
+let body=document.querySelector('.outer');
 
-// body.style.backgroundColor='red'
+let flag=false;
 btn.addEventListener('click',()=>{
-	modal.showModal();
-	setTimeout(()=> body.addEventListener('click', ()=> modal.close()), 3000)
+	modal.style.display='inline'
+	flag=true
 	
 	
 })
+body.addEventListener('click', (e)=>{
+	// e.preventDefault()
+	if(flag){
+	modal.style.display='none'
+		
+	}
+})
 
+function down() {
+	modal.style.display='none'
+	
+}
 
